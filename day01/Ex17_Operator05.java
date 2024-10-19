0041d86d630d4e5cde96f8e8e5bbc64d7854ea49
soyeon1806 <ksy1118s@naver.com>
2024-10-20 02:26:46 +0900

    feat: Added example of assignment and compound assignment operators

---
+++ day01/Ex17_Operator05.java
@@ -0,0 +1,11 @@
+public class Ex17_Operator05 {
+    public static void main(String[] args) {
+        // 대입 (할당) 연산자
+        int a = 10;
+        System.out.println(a);
+
+        // 복합 대입 연산자 (산술 연산자 + 대입 연산자)
+        a += 10;
+        System.out.println(a);
+    }
+}
