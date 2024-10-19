5db766ceec53ef217a16bdd490f288e84e08b007
soyeon <ksy1118s@naver.com>
2024-10-19 18:45:21 +0900

    docs: Added comments explaining potential code changes after compilation

--- day01/Ex03_Variable01.java
+++ day01/Ex03_Variable01.java
@@ -4,5 +4,14 @@ public class Ex03_Variable01 {
         
         num = 10;
         System.out.println(num);
+
+        num = 20;
+        System.out.println(num);
     }
 }
+
+
+/*
+ * 이 코드를 컴파일한 후, 디컴파일을 하면 원본 코드와 다르게 보임
+ * -> Java 컴파일러가 코드 최적화를 적용하기 때문
+ */
