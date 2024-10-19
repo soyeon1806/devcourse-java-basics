a7b0d3a244bc23f4935878d125477b41bb4c0023
soyeon1806 <ksy1118s@naver.com>
2024-10-20 02:15:10 +0900

    feat: Added examples of integer and floating-point division

---
+++ day01/Ex13_Operator01.java
@@ -0,0 +1,16 @@
+public class Ex13_Operator01 {
+    public static void main(String[] args) {
+        // 정수 / 정수 = 정수
+        System.out.println(3 / 2);
+        System.out.println(5 / 2);
+
+        // 정수 / 실수 = 실수
+        System.out.println(5 / 2.0);
+
+        int i1 = 5;
+        int i2 = 2;
+        // int result = i1 / i2; // <- 정수로 나옴
+        double result = i1 / (double)i2;
+        System.out.println(result);
+    }
+}
