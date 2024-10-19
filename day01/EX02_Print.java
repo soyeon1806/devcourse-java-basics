e6b1aa6173174610a1a189f5ba48bf8e53a31db3
soyeon <ksy1118s@naver.com>
2024-10-19 18:29:11 +0900

    chore: rename files

--- day01/EX02_Print.java
+++ day01/EX02_Print.java
@@ -1,4 +1,4 @@
-public class EX02_Print {
+public class Ex02_Print {
     public static void main(String[] args) {
         System.out.println("안녕 자바");
     }
---
+++ day01/Ex03_Variable.java
@@ -0,0 +1,8 @@
+public class Ex03_Variable {
+    public static void main(String[] args){
+        int num;
+        
+        num = 10;
+        System.out.println(num);
+    }
+}
