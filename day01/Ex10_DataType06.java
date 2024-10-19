4983e9bf5528fe1e96940e03af23f7083b035437
soyeon1806 <ksy1118s@naver.com>
2024-10-20 02:00:59 +0900

    feat: Added example of float and double usage with scientific notation

---
+++ day01/Ex10_DataType06.java
@@ -0,0 +1,19 @@
+public class Ex10_DataType06 {
+    public static void main(String[] args){
+        /*
+         * 실수
+         * float
+         * double
+         */
+
+        float f = 2.5f;
+        System.out.println(f);
+
+        double d = 2.5;
+        System.out.println(d);
+
+        // 지수 표기법(Scientific Notation)을 사용한 double 타입
+        double d2 = 1.0e3;
+        System.out.println(d2);
+    }
+}
