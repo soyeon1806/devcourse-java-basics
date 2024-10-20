e5b2a460f6608a0c481de8e6a3c2553e74c314d5
soyeon <ksy1118s@naver.com>
2024-10-20 18:55:31 +0900

    feat: Added example of multiplication table using for loop

---
+++ day02/Ex12_Loop03.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Ex12_Loop03 {
+    public static void main(String[] args) {
+        int num = 3;
+
+        for(int i = 1; i < 10; i++){
+            System.out.println(num + " X " + i + " = " + num * i);
+        }
+    }
+}
