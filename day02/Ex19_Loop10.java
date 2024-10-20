3a36d3ab518bba3bb3f69f25f15a0d236cfc7b52
soyeon <ksy1118s@naver.com>
2024-10-20 20:23:45 +0900

    feat: Added example of while loop to print alphabet from A to Z using ASCII codes

---
+++ day02/Ex19_Loop10.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Ex19_Loop10 {
+    public static void main(String[] args) {
+        int i = 65;
+        while(i <= 90){
+            System.out.println((char)i);
+            i++;
+        }
+    }
+}
