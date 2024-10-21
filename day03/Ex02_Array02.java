68c4b0c427e838520638b39b5509c4927caf1eca
soyeon1806 <ksy1118s@naver.com>
2024-10-21 10:36:48 +0900

    feat: Added example of array declaration, initialization, and usage

---
+++ day03/Ex02_Array02.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class Ex02_Array02 {
+    public static void main(String[] args) {
+        // 선언, 생성, 초기화 동시에
+        int[] arr = new int[] {10, 20, 30, 40, 50};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[4]);
+
+
+        int[] ary = {10, 20, 30, 40, 50};
+        
+        System.out.println(ary[0]);
+        System.out.println(ary[4]);
+    }
+}
