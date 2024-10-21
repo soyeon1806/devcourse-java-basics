8a771153114cd537079bc074f0c54fdad5cee900
soyeon1806 <ksy1118s@naver.com>
2024-10-21 12:49:58 +0900

    feat: Added example of defining a class with member fields and methods in Java

---
+++ day03/Ex16_Method03.java
@@ -0,0 +1,39 @@
+package day03;
+
+class Person{
+    // 클래스는 멤버 필드와 메서드로 구성되어 있음
+
+    // 멤버 필드
+    String name;
+    int age;
+    String jumin;
+    double height;
+    double weight;
+
+    // 메서드
+    void smile(){
+        System.out.println("웃다");
+    }
+
+    void eat() {
+        System.out.println("먹다");
+    }
+
+    void sleep() {
+        System.out.println("자다");
+    }
+}
+
+public class Ex16_Method03 {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "이순신";
+        p1.age = 44;
+        p1.jumin = "111";
+        p1.height = 182.0;
+        p1.weight = 85;
+
+        p1.smile();
+        p1.eat();
+    }
+}
