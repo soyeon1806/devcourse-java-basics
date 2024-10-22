b4c1da0e4ae7c977884553b002217f37ef2cb8ba
soyeon <ksy1118s@naver.com>
2024-10-22 20:20:46 +0900

    refactor: Renamed class GenesisCar to GenesisCar03

--- day04/Prac03_SuperConstructor.java
+++ day04/Prac03_SuperConstructor.java
@@ -14,14 +14,14 @@ class Car03 {
     }
 }
 
-class GenesisCar extends Car03{
+class GenesisCar03 extends Car03{
     int price;
     
     /**
         1. 매개변수 4개를 받는 생성자 선언하기
             - super를 사용해서 부모 클래스의 필드를 초기화하세요.
     */
-    GenesisCar(int speed, boolean operable, char grade, int price) {
+    GenesisCar03(int speed, boolean operable, char grade, int price) {
         super(speed, operable, grade);
         this.price = price;
     }
@@ -33,7 +33,7 @@ class GenesisCar extends Car03{
             - price는 0으로 초기화하세요
             - (또는 this를 사용해서 생성자 오버로딩으로 완성해도 됩니다.)
     */
-    GenesisCar(int speed, boolean operable, char grade) {
+    GenesisCar03(int speed, boolean operable, char grade) {
         super(speed, operable, grade);
         this.price = 0;
     }
@@ -44,7 +44,7 @@ class GenesisCar extends Car03{
             - super를 사용해서 부모 클래스의 필드를 초기화하세요.
             - price는 0으로 초기화하세요
     */
-    GenesisCar(int speed, boolean operable) {
+    GenesisCar03(int speed, boolean operable) {
         super(speed, operable);
         this.price = 0;
     }
@@ -52,7 +52,7 @@ class GenesisCar extends Car03{
 
 public class Prac03_SuperConstructor {
     public static void main(String[] args) {
-        GenesisCar genesisCar = new GenesisCar(150, true);
+        GenesisCar03 genesisCar = new GenesisCar03(150, true);
         System.out.println(genesisCar.speed);
         System.out.println(genesisCar.operable);
         System.out.println(genesisCar.grade);
