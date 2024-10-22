2dc06f5f7982a2c5cf421db60f90d61b98daf894
soyeon <ksy1118s@naver.com>
2024-10-22 20:58:53 +0900

    feat: Added example demonstrating polymorphism and field hiding with method overriding in Java

---
+++ day05/Ex01_Poly01.java
@@ -0,0 +1,91 @@
+package day05;
+
+// 부모 클래스
+class Parent{
+    String data = "박문수"; // 부모 클래스의 멤버 변수
+
+    // 부모 클래스의 생성자
+    Parent(){
+        System.out.println("Parent 생성자 호출");
+    }
+
+    // 부모 클래스의 메서드 (오버라이딩 가능)
+    void viewData1(){
+        System.out.println("Parent viewData1() 호출");
+    }
+
+    // 부모 클래스의 메서드 (오버라이딩 가능)
+    void viewData2(){
+        System.out.println("Parent viewData2() 호출");
+    }
+}
+
+// 자식 클래스 1
+class Child1 extends Parent{
+    String data = "홍길동"; // 자식 클래스의 멤버 변수 (부모와 같은 이름, 하지만 오버라이딩 아님)
+
+    // 자식 클래스 1의 생성자
+    Child1(){
+        System.out.println("Child1 생성자 호출");
+    }
+
+    // 부모 클래스의 메서드를 오버라이딩
+    @Override
+    void viewData1() {
+        System.out.println("Child1 viewData1() 호출");
+    }
+
+    // 부모 클래스의 메서드를 오버라이딩
+    @Override
+    void viewData2(){
+        System.out.println("Child1 viewData2() 호출");
+    }
+}
+
+// 자식 클래스 2
+class Child2 extends Parent{
+    // 자식 클래스 2의 생성자
+    Child2(){
+        System.out.println("Child2 생성자 호출");
+    }
+
+    // 부모 클래스의 메서드를 오버라이딩
+    @Override
+    void viewData1(){
+        System.out.println("Child2 viewData1() 호출");
+    }
+
+    // 부모 클래스의 메서드를 오버라이딩
+    @Override
+    void viewData2(){
+        System.out.println("Child2 viewData2() 호출");
+    }
+}
+
+public class Ex01_Poly01 {
+    public static void main(String[] args) {
+        // 부모 클래스를 사용한 객체 생성
+        Parent p1 = new Parent();
+        System.out.println(p1.data);
+        p1.viewData1();
+        p1.viewData2();
+
+        System.out.println();
+
+        // 자식 클래스를 부모 타입으로 참조 (다형성)
+        /*
+         * 참조 변수 p2는 Parent 타입이기 때문에
+         * p2.data를 호출하면 Parent 클래스의 data 필드가 참조됨
+         */
+        Parent p2 = new Child1();
+        System.out.println(p2.data); // 부모 클래스의 멤버 변수를 참조 (오버라이딩 아님)
+        p2.viewData1(); // 자식 클래스에서 오버라이딩된 메서드 호출 (다형성)
+        p2.viewData2(); // 자식 클래스에서 오버라이딩된 메서드 호출 (다형성)
+
+        System.out.println();
+
+        p2 = new Child2();
+        p2.viewData1(); // 자식 클래스에서 오버라이딩된 메서드 호출 (다형성)
+        p2.viewData2(); // 자식 클래스에서 오버라이딩된 메서드 호출 (다형성)
+    }
+}
