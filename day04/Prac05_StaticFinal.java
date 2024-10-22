8024a8cd69740cd1d52a1823098f017416a24ee0
soyeon <ksy1118s@naver.com>
2024-10-22 20:13:20 +0900

    feat: Added static final constant MAX_RPM

---
+++ day04/Prac05_StaticFinal.java
@@ -0,0 +1,16 @@
+class Car05 {
+    /**
+        클래스 상수를 선언하세요. (static final 활용)
+        - 필드명은 MAX_RPM
+        - 정수 타입으로 선언하고 값은 300으로 초기화합니다.
+        
+    */
+    
+    static final int MAX_RPM = 300;
+}
+
+public class Prac05_StaticFinal {
+    public static void main(String[] args) {
+        System.out.println(Car05.MAX_RPM);
+    }
+}
