728eb80941f5c9de91ab48b8646e25f0549716e7
soyeon <ksy1118s@naver.com>
2024-10-22 21:25:44 +0900

    chore: Updated comments

--- day05/Ex01_Poly01.java
+++ day05/Ex01_Poly01.java
@@ -77,6 +77,13 @@ public class Ex01_Poly01 {
          * 참조 변수 p2는 Parent 타입이기 때문에
          * p2.data를 호출하면 Parent 클래스의 data 필드가 참조됨
          */
+        /*
+         * Parent01 p2 = new Child01_1();에서 자식 객체를 부모 클래스 타입으로 참조함
+         * -> 이는 암묵적으로 허용되며, 자바의 다형성을 지원함
+         * 
+         * 오버라이딩된 메서드는 실제 객체 타입인 Child01의 메서드가 호출되지만,
+         * 필드는 참조 타입에 따라 Parent01 클래스의 값이 출력됨
+         */
         Parent01 p2 = new Child01_1();
         System.out.println(p2.data); // 부모 클래스의 멤버 변수를 참조 (오버라이딩 아님)
         p2.viewData1(); // 자식 클래스에서 오버라이딩된 메서드 호출 (다형성)
