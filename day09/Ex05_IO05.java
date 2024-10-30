56528f6ebd6ea9f0a82f30c943d79ebe0764bf7c
soyeon <ksy1118s@naver.com>
2024-10-30 19:22:27 +0900

    chore: Add comments

--- day09/Ex05_IO05.java
+++ day09/Ex05_IO05.java
@@ -4,6 +4,12 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * Ex05_IO05 클래스
+ * - DataInputStream을 사용하여 바이너리 형식으로 저장된 데이터를 읽어오는 예제
+ * - data.dat 파일로부터 정수, 문자열, 실수를 순서대로 읽어옴
+ */
+
 public class Ex05_IO05 {
     public static void main(String[] args) {
         DataInputStream dis = null;
